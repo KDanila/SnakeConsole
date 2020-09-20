827e7147d350c3c500c0c23b29dc94df45b7cd29
KDanila <concista@gmail.com>
2020-09-20 10:15:54 +0300

    Add temp nextStep.

--- src/main/ru/kdv/model/Snake.java
+++ src/main/ru/kdv/model/Snake.java
@@ -10,4 +10,21 @@ public class Snake {
     private final String tail = "*";
     private final LinkedList<Point> positions = new LinkedList<>();
     private Direction direction;
+    private final Board board;
+
+    private void nextStep() {
+        Point head = positions.getFirst();
+        //todo if next position dote -> add point
+
+        //todo border of board %size of board
+        Point newHead = switch (direction) {
+            case UP -> new Point(head.getX(), head.getY() + 1, true);
+            case DOWN -> new Point(head.getX(), head.getY() - 1, true);
+            case LEFT -> new Point(head.getX() - 1, head.getY(), true);
+            case RIGHT -> new Point(head.getX() + 1, head.getY(), true);
+        };
+
+        positions.addFirst(newHead);
+        positions.removeLast();
+    }
 }
